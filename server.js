be3fe986a6ed7f1eeb691f5ecb5694cf12082222
MichaelTreat <michael.leon.treat@gmail.com>
2017-12-19 10:00:08 -0800

    Completed the initial build of the server file. Test and it successfully passes a build. Next I will start working on the model.

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const cors = require('cors')
+const pg = require('pg')
+
+// the ||'s are local dev setups.s
+const app = express()
+const PORT = process.env.PORT || 3000
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhoast:8080'
+
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://postgres:Mt145824@localhost:5432/book_app' )
+client.connect()
+client.on('error', err => console.error(err))
+
+app.use(cors())
+
+app.listen(PORT, () => console.log(`listening on port :${PORT}`) )
