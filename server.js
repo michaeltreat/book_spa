9cbed68f70af714ece897a4c3f0c15a306238d6e
MichaelTreat <michael.leon.treat@gmail.com>
2017-12-19 19:05:36 -0800

    added a simple all route and fixed a type in the CLIENT_URL || localhost string.

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const pg = require('pg')
 // the ||'s are local dev setups.s
 const app = express()
 const PORT = process.env.PORT || 3000
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhoast:8080'
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080'
 
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://postgres:Mt145824@localhost:5432/book_app' )
 client.connect()
 client.on('error', err => console.error(err))
 
 app.use(cors())
+
+app.all('*', (req, res) => res.redirect(CLIENT_URL))
+
+
 
 app.listen(PORT, () => console.log(`listening on port :${PORT}`) )
